a5d43f4dc2b9bc03e0bc076db2928e9bd1c16425
denisa_biris <denisa_biris@yahoo.com>
2017-12-31 00:26:22 +0200

    butoane de stergere in tabel

--- js/introducereReteta.js
+++ js/introducereReteta.js
@@ -3,6 +3,12 @@ $( "#adaugareMedicamentNou" ).click(function() {
         '<td><input type="text" class="form-control" value="" /></td>' +
         '<td><input type="text" class="form-control" value="" /></td>' +
         '<td><input type="text" class="form-control" value="" /></td>' +
+        '<td><button type="button" onclick="deleteRowFunction(this)" class="btn btn-lg btn-danger">Șterge</button></td>' +
         '</tr>';
     $(tblRow).appendTo("#introducereReteta tbody");
 });
+
+function deleteRowFunction(o) {
+    var p=o.parentNode.parentNode;
+    p.parentNode.removeChild(p);
+}
