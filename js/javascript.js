6015a25f12f832cfdee182cbf5837eb5086ad19b
denisa_biris <denisa_biris@yahoo.com>
2018-01-03 16:31:53 +0200

    Cazul 6 - adaugare comentarii la tratament

--- js/javascript.js
+++ js/javascript.js
@@ -138,3 +138,17 @@ function trimite() {
 		document.getElementById("#infoText").innerHTML = "Datele au fost introduse corect!";
 	}
 }
+
+$( "#adaugaComentariuButton" ).click(function() {
+    var adaugaComentariuInput = $("#adaugaComentariuInput").val();
+    if(adaugaComentariuInput!=""){
+		$("#adaugaComentariuInput").val("");
+
+		var comentariuNou = '<li>' +
+			'<div class="mr-2 float-left"> <img src="http://placekitten.com/50/50" /></div>' +
+			'<p class="m-0">'+adaugaComentariuInput+'</p>' +
+			'<small class="date text-muted">16 Ianuarie 2018</small>' +
+			'</li>';
+		$(comentariuNou).prependTo("#listaComentarii");
+    }
+});
