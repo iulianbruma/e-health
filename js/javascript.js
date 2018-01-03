f0c3160b936b956ab65641d965983a22b155fb12
unknown <ralucaflorinalazar@gmail.com>
2018-01-03 19:44:21 +0200

    Caz 11 - Adaugare conferinta

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,9 @@
-
+//Modal - distribuie conferinta adaugata
+var shareModal = document.getElementById('shareModal');
+var btnDistribuie = document.getElementById("distribuie");
+var btnSalveaza = document.getElementById("salveaza");
+var btnDistribuie1 = document.getElementById("distribuie1");
+var spanClose = document.getElementsByClassName("close")[0];
 
 $( "#loginId" ).click(function() {
 	var username = document.getElementById("username").value;
@@ -137,4 +142,30 @@ function trimite() {
 	if (nume && adresa && data && telefon && email) {
 		document.getElementById("#infoText").innerHTML = "Datele au fost introduse corect!";
 	}
+}
+
+btnSalveaza.onclick = function() {
+    alert('Conferința a fost salvată cu succes');
+}
+
+// When the user clicks on the button, open the modal 
+btnDistribuie.onclick = function() {
+    shareModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanClose.onclick = function() {
+    shareModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == shareModal) {
+        shareModal.style.display = "none";
+    }
+}
+
+btnDistribuie1.onclick = function() {
+    alert('Conferința a fost distribuită cu succes!');
+    shareModal.style.display = "none";
 }
