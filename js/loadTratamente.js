6e943b1ff6f89fdeab79eb33e61baa8cf175108f
unknown <anda.dregan@yahoo.com>
2018-01-05 17:50:37 +0200

    Caz3- Vizualizare tratament + caz alternativ

--- js/loadTratamente.js
+++ js/loadTratamente.js
@@ -14,3 +14,32 @@ $(document).ready(function() {
 		$("<tbody/>", {html: tratamente.join("")}).appendTo('.tabelTratamente');
     });
 });
+
+
+
+
+ $(".cautaMedicament").on("click", function(){
+
+		var tratamente = [];
+        var medicam = $(".cautaMedicamInput").val();
+        var mediciTable = $(".tabelTratamente tbody tr").remove();
+       $.getJSON('data/tratam.json', function(data) {
+           $.each(data.tratamente, function(i, f) {
+
+                if(f.medicament.includes(medicam) ){
+					tratamente.push("<tr>");
+					tratamente.push("<td>" + f.nrCrt + "</td>");
+					tratamente.push("<td>" + f.dataConsultatiei + "</td>");
+					tratamente.push("<td>" + f.medic + "</td>");
+					tratamente.push("<td>" + f.diagnostic + "</td>");
+					tratamente.push('<td><button type="button" onclick="window.location.href=\'vizTrat2.html\'" class="btn btn-lg btn-success">Vizualizare</button></td>');
+					tratamente.push("<tr>");
+					$("<tbody/>", {html: tratamente.join("")}).appendTo('.tabelTratamente');
+               }
+           });
+       });
+
+       return false;
+
+ });
+ 
