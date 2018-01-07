06c3cdb9912f0d83c3ab675f51e825fc273ab9d7
denisa_biris <denisa_biris@yahoo.com>
2018-01-07 20:26:24 +0200

    Cazul 4 - introducere reteta - cazul de eroare si loading

--- js/introducereReteta.js
+++ js/introducereReteta.js
@@ -3,11 +3,31 @@ $(document).ready(function() {
 
     $("#numePacient").text("Nume: "+urlParams.get("nume")+" "+urlParams.get("prenume")+".");
     $("#varstaPacient").text("Vârsta: "+urlParams.get("varsta")+".");
+
+    $("#introducereRetetaButon").click(function(){
+        $("div#divLoading").addClass('show');
+        setTimeout( function(){
+            $("div#divLoading").removeClass('show');
+
+            var succes = true;
+            $.each($(".numeMedicament"), function(i, f) {
+                if($(f).val().includes("algocalmin")){
+                    succes = false;
+                }
+            });
+            if(succes){
+                $("#introducereRetetaModalSucces").modal();
+            }
+            else {
+                $("#introducereRetetaModalEroare").modal();
+            }
+        }, 1000);
+    });
 });
 
 $( "#adaugareMedicamentNou" ).click(function() {
     var tblRow = '<tr>' +
-        '<td><input type="text" class="form-control" value="" /></td>' +
+        '<td><input type="text" class="form-control numeMedicament" value="" /></td>' +
         '<td><input type="text" class="form-control" value="" /></td>' +
         '<td><input type="text" class="form-control" value="" /></td>' +
         '<td><button type="button" onclick="deleteRowFunction(this)" class="btn btn-lg btn-danger">Șterge</button></td>' +
