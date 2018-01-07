4f7c8cfa3fbc7bdf3e053f36ac5dbe25354f2306
denisa_biris <denisa_biris@yahoo.com>
2018-01-08 00:10:09 +0200

    Cazul 6 - adaugare comentarii la tratament - cazul de eroare si loading

--- js/javascript.js
+++ js/javascript.js
@@ -75,20 +75,6 @@ $(function () {
     $('#datetimepicker1').datetimepicker();
 });
 
-$( "#adaugaComentariuButton" ).click(function() {
-    var adaugaComentariuInput = $("#adaugaComentariuInput").val();
-    if(adaugaComentariuInput!=""){
-		$("#adaugaComentariuInput").val("");
-
-		var comentariuNou = '<li>' +
-			'<div class="mr-2 float-left"> <img src="http://placekitten.com/50/50" /></div>' +
-			'<p class="m-0">'+adaugaComentariuInput+'</p>' +
-			'<small class="date text-muted">16 Ianuarie 2018</small>' +
-			'</li>';
-		$(comentariuNou).prependTo("#listaComentarii");
-    }
-});
-
 function resetFields() {
 	var inputs = document.querySelectorAll("input[type=text]");
 
