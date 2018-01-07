b97bb1cf5579c91ca829fc06180fa61486a21247
denisa_biris <denisa_biris@yahoo.com>
2018-01-07 17:17:02 +0200

    refactorizare loadPacienti si loadMedici

--- js/loadMedici.js
+++ js/loadMedici.js
@@ -5,46 +5,34 @@ $(document).ready(function() {
         buttonOnClickHref = "adaugare_feedback2.html";
     }
 
-    var medici = [];
-
-    $.getJSON('data/medici.json', function(data) {
-        $.each(data.medici, function(i, f) {
-            var tblRow = "<tr>" +
-                "<td>" + f.nrCrt + "</td>" +
-                "<td>" + f.nume + "</td>" +
-                "<td>" + f.ocupatie + "</td>" +
-                "<td>" + f.nota + "</td>" +
-                "<td class='text-warning'>" + f.stele + "</td>" +
-                '<td><button type="button" onclick="window.location.href=\''+buttonOnClickHref+'?nume='+f.nume+'\'" class="btn btn-lg btn-success">Adaugă recenzii</button></td>' +
-                "</tr>";
-            $(tblRow).appendTo(".mediciTable tbody");
-        });
-
-    });
-
     $(".searchMedic").on("submit", function(){
         var searchMedicInput = $(".searchMedicInput").val();
 
         var mediciTable = $(".mediciTable tbody tr").remove();
 
-        $.getJSON('data/medici.json', function(data) {
-            $.each(data.medici, function(i, f) {
-                if(f.nume.includes(searchMedicInput)){
-                    var tblRow = "<tr>" +
-                        "<td>" + f.nrCrt + "</td>" +
-                        "<td>" + f.nume + "</td>" +
-                        "<td>" + f.ocupatie + "</td>" +
-                        "<td>" + f.nota + "</td>" +
-                        "<td class='text-warning'>" + f.stele + "</td>" +
-                        '<td><button type="button" onclick="window.location.href=\''+buttonOnClickHref+'?nume='+f.nume+'\'" class="btn btn-lg btn-success">Adaugă recenzii</button></td>' +
-                        "</tr>";
-                    $(tblRow).appendTo(".mediciTable tbody");
-                }
-            });
-
-        });
+        loadTable(buttonOnClickHref,searchMedicInput);
 
         return false;
-    });
-
+    }).submit();
 });
+
+function loadTable(buttonOnClickHref, searchMedicInput){
+    var medici = [];
+
+    $.getJSON('data/medici.json', function(data) {
+        $.each(data.medici, function(i, f) {
+            if(f.nume.includes(searchMedicInput)){
+                var tblRow = "<tr>" +
+                    "<td>" + f.nrCrt + "</td>" +
+                    "<td>" + f.nume + "</td>" +
+                    "<td>" + f.ocupatie + "</td>" +
+                    "<td>" + f.nota + "</td>" +
+                    "<td class='text-warning'>" + f.stele + "</td>" +
+                    '<td><button type="button" onclick="window.location.href=\''+buttonOnClickHref+'?nume='+f.nume+'\'" class="btn btn-lg btn-success">Adaugă recenzii</button></td>' +
+                    "</tr>";
+                $(tblRow).appendTo(".mediciTable tbody");
+            }
+        });
+
+    });
+}
--- js/loadPacienti.js
+++ js/loadPacienti.js
@@ -8,48 +8,35 @@ $(document).ready(function() {
         buttonOnClickHref = "vizualizare_date2.html";
     }
 
-    var pacienti = [];
-
-    $.getJSON('data/pacienti.json', function(data) {
-        $.each(data.pacienti, function(i, f) {
-            var tblRow = "<tr>" +
-                "<td>" + f.nrCrt + "</td>" +
-                "<td>" + f.nume + "</td>" +
-                "<td>" + f.prenume + "</td>" +
-                "<td>" + f.email + "</td>" +
-                "<td>" + f.dataNasterii + "</td>" +
-                "<td>" + f.adresa + "</td>" +
-                '<td><button type="button" onclick="window.location.href=\''+buttonOnClickHref+'?nume='+f.nume+'&prenume='+f.prenume+'&varsta='+(2017-parseInt(f.dataNasterii.split(".").pop()))+'\'" class="btn btn-lg btn-success">Alege</button></td>' +
-                "</tr>";
-            $(tblRow).appendTo(".pacientiTable tbody");
-        });
-
-    });
-
     $(".searchPacient").on("submit", function(){
         var searchPacientInput = $(".searchPacientInput").val();
 
         var pacientiTable = $(".pacientiTable tbody tr").remove();
 
-        $.getJSON('data/pacienti.json', function(data) {
-            $.each(data.pacienti, function(i, f) {
-                if(f.nume.includes(searchPacientInput) || f.prenume.includes(searchPacientInput)){
-                    var tblRow = "<tr>" +
-                        "<td>" + f.nrCrt + "</td>" +
-                        "<td>" + f.nume + "</td>" +
-                        "<td>" + f.prenume + "</td>" +
-                        "<td>" + f.email + "</td>" +
-                        "<td>" + f.dataNasterii + "</td>" +
-                        "<td>" + f.adresa + "</td>" +
-                        '<td><button type="button" onclick="window.location.href=\''+buttonOnClickHref+'?nume='+f.nume+'&prenume='+f.prenume+'&varsta='+(2017-parseInt(f.dataNasterii.split(".").pop()))+'\'" class="btn btn-lg btn-success">Alege</button></td>' +
-                        "</tr>";
-                    $(tblRow).appendTo(".pacientiTable tbody");
-                }
-            });
-
-        });
+        loadTable(buttonOnClickHref,searchPacientInput);
 
         return false;
-    });
-
+    }).submit();
 });
+
+function loadTable(buttonOnClickHref, searchPacientInput){
+    var pacienti = [];
+
+    $.getJSON('data/pacienti.json', function(data) {
+        $.each(data.pacienti, function(i, f) {
+            if(f.nume.includes(searchPacientInput) || f.prenume.includes(searchPacientInput)){
+                var tblRow = "<tr>" +
+                    "<td>" + f.nrCrt + "</td>" +
+                    "<td>" + f.nume + "</td>" +
+                    "<td>" + f.prenume + "</td>" +
+                    "<td>" + f.email + "</td>" +
+                    "<td>" + f.dataNasterii + "</td>" +
+                    "<td>" + f.adresa + "</td>" +
+                    '<td><button type="button" onclick="window.location.href=\''+buttonOnClickHref+'?nume='+f.nume+'&prenume='+f.prenume+'&varsta='+(2017-parseInt(f.dataNasterii.split(".").pop()))+'\'" class="btn btn-lg btn-success">Alege</button></td>' +
+                    "</tr>";
+                $(tblRow).appendTo(".pacientiTable tbody");
+            }
+        });
+
+    });
+}
